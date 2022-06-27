7186610501b16e84cb9742b3f4d3d7d04aa44e22
Guilherme H. Santos <guilherme.hxsantos@gmail.com>
2022-06-27 19:14:55 -0300

    Ignite-Lab-App

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,77 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { Link, useParams } from 'react-router-dom';
+
+import classNames from 'classnames';
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson (props: LessonProps) {
+    const { slug } = useParams<{slug: string}>();
+
+    const isLessonAvailable = isPast(props.availableAt);
+    const availableDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+        locale: ptBR,
+    })
+
+    const isActiveLesson = slug === props.slug;
+
+    return (
+        <Link to={`/event/lesson/${props.slug}`} className='group'>
+            <span className="text-gray-300">
+                {availableDateFormated}
+            </span>
+            <div 
+                className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
+                    'bg-green-500': isActiveLesson,
+                })}
+            >
+                <header className="flex items-center justify-between">
+                    {isLessonAvailable ? (
+                        <span 
+                            className={classNames('text-sm font-medium flex items-center gap-2',{
+                                'text-blue-500': !isActiveLesson,
+                                'text-white': isActiveLesson,
+                            })}
+                        >
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>
+                    ) :
+                        <span 
+                            className={classNames('text-sm font-medium flex items-center gap-2',{
+                                'text-orange-500': !isActiveLesson,
+                                'text-white': isActiveLesson,
+                            })}
+                        >
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    }
+                    <span 
+                        className={classNames('text-xs rounded py-[0.125rem] px-2 text-white border font-bold',{
+                            'border-green-300': !isActiveLesson,
+                            'border-white': isActiveLesson,
+                        })}
+                    >
+                        {props.type === 'live' ? 'AO VIVO!' : 'AULA PRÁTICA'}
+                    </span>
+                </header>
+                <strong 
+                    className={classNames('mt-5 block',{
+                        'text-gray-200': !isActiveLesson,
+                        'text-white': isActiveLesson,
+                    })}
+                >
+                    {props.title}
+                </strong>
+            </div>
+        </Link>
+    )
+}
